d78c001d23183889d59133b423e9800f2b424129
Kimhyeyun <cqqwer@gmail.com>
2023-12-13 22:29:22 +0900

    init

---
+++ src/main/java/com/sparta/springchallengeassignment/SpringChallengeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springchallengeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChallengeAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChallengeAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springchallengeassignment/SpringChallengeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springchallengeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChallengeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
