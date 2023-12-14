64ffaaea52c37838f5d8bd29510ed13e63c5cb78
Kimhyeyun <cqqwer@gmail.com>
2023-12-14 14:26:04 +0900

    feat: 에러 응답 객체/코드 생성

---
+++ src/main/java/com/sparta/springchallengeassignment/exception/ApiException.java
@@ -0,0 +1,20 @@
+package com.sparta.springchallengeassignment.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ApiException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+    private final String message;
+
+    public ApiException(ErrorCode errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public ApiException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getDetail();
+    }
+}
