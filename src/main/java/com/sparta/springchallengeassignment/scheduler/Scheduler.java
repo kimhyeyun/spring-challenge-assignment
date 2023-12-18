d1ac791e2ef346fd5ad96b2e0777395758d3cb21
Kimhyeyun <cqqwer@gmail.com>
2023-12-18 19:41:35 +0900

    feat : 게시글 삭제 스케줄러 기능 구현/매일 자정 기준 60일이 지난 게시글 삭제 하도록 함

---
+++ src/main/java/com/sparta/springchallengeassignment/scheduler/Scheduler.java
@@ -0,0 +1,34 @@
+package com.sparta.springchallengeassignment.scheduler;
+
+import com.sparta.springchallengeassignment.domain.Post;
+import com.sparta.springchallengeassignment.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Scheduler {
+
+    private final PostService postService;
+
+    @Scheduled(cron = "0 0 0 * * *")
+//    @Scheduled(cron = "* 38 19 * * *")
+    public void deletePost() {
+        LocalDateTime now = LocalDateTime.now();
+
+        List<Post> posts = postService.getPosts();
+        for (Post post : posts) {
+            long duration = ChronoUnit.DAYS.between(post.getModifiedAt(), now);
+//            long duration = ChronoUnit.SECONDS.between(post.getModifiedAt(), now);
+            if (duration >= 60) {
+                postService.delete(post);
+            }
+        }
+    }
+}
